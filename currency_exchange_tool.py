dfb38ccee3887ebe9292d792f32bfdfeab800bb0
wc294 <wayne.ching@laetottenham.ac.uk>
2024-10-10 10:49:59 +0100

    Update currency_exchange_tool.py

--- currency_exchange_tool.py
+++ currency_exchange_tool.py
@@ -1,8 +1,12 @@
 import math # you'll probably need this
 
 exchange_rates = {
-    'USD': 1.13, #I.E. 1 Pound is 1.13 Dollars
-    'EUR': 1.15,
+    'USD': 1.31, #I.E. 1 Pound is 1.13 Dollars
+    'EUR': 1.20,
+    'GBP':1,
+    'CAD':1.80,
+    'BDT':119.78,
+    'CNY':9.25}
 }
 
 def check_currency_exists(currency):
