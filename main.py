d053a3f0bf7483b9706f9954b8870644a90ffd33
wc294 <wayne.ching@laetottenham.ac.uk>
2024-10-10 10:11:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from currency_exchange_tool import currency_convert
 from shop_functions import start_shop
 
+currencies={
+    'GBP':1,
+    'USD':0.77,
+    'CAD':0.56,
+    'BDT':0.0064,
+    'CNY':0.11}
 def forex():
     
 print('Welcome to my shop')
