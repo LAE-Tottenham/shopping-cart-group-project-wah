30ac33acd7e5439a431a5909bdcca4530b7ed52b
hamim-a77 <hamim.ahnaf@laetottenham.ac.uk>
2024-11-05 20:45:15 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from currency_exchange_tool import currency_convert
-from shop_functions import start_shop
+from shop_functions import shopping_cart
 
 print('Welcome to my shop')
 
 while True:
     print('Please select what you would like to buy')
-    items_to_buy = start_shop()
+    items_to_buy = shopping_cart()
 
 
